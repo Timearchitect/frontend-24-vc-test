bec52ab5ce64a70b9bcd4904cae4bcd325f7dd54
Alrik He <alrik_he@hotmail.com>
2025-04-01 02:26:33 +0200

    api key

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-require('dotenv').config();
-const apiKey = process.env.apiKey;
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const apiKey = process.env.apikey;
 console.log(`API Key: ${apiKey}`);
